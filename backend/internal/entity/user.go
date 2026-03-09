1fcd4656e772b19916b62f303efa97e7663c1ba2
M. Yahya Muhaimin <surat.pribadiku@gmail.com>
2026-03-09 14:43:58 +0700

    [be] feat: add db tag to User entity

--- backend/internal/entity/user.go
+++ backend/internal/entity/user.go
@@ -1,8 +1,8 @@
 package entity
 
 type User struct {
-	ID           string `json:"id"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"-"`
-	Role         string `json:"role"`
+	ID           string `db:"id" json:"id"`
+	Email        string `db:"email" json:"email"`
+	PasswordHash string `db:"password_hash" json:"-"`
+	Role         string `db:"role" json:"role"`
 }
