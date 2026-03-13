e1dc742dfb2e992111eb05eb43167bda1995bb66
M. Yahya Muhaimin <surat.pribadiku@gmail.com>
2026-03-13 23:20:05 +0700

    [fe] chore: update build targets

--- frontend/quasar.config.ts
+++ frontend/quasar.config.ts
@@ -33,8 +33,8 @@ export default defineConfig((/* ctx */) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
       target: {
-        browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
-        node: 'node20',
+        browser: ['es2022', 'firefox115', 'chrome115', 'safari16'],
+        node: 'node22',
       },
 
       typescript: {
